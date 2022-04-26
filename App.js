6fa60d6be7c6454c952a375bf6101c2643f75fd0
Dostonbek Davlatov <67422136+Nick0422@users.noreply.github.com>
2022-04-25 23:32:53 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import react from "react";
+import { View, SafeAreaView, Button} from "react-native";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+
+export default function App() {
+return(
+  
+  <WelcomeScreen />
+  );
+}
